44b9987a55017e9a472ae1712a120ccc34f8f602
ibecker1 <ignacio.becker@ug.uchile.cl>
2020-04-28 04:27:35 +0000

    edita script

--- Scripts/Processing/limpieza_bbdd.R
+++ Scripts/Processing/limpieza_bbdd.R
@@ -10,8 +10,12 @@ lapply(x, require, character.only = TRUE)
 
 ene <- read_dta('http://www.ine.cl/docs/default-source/ocupacion-y-desocupacion/bbdd/2019/formato-stata/ene-2019-04.dta?sfvrsn=599190e8_6&download=true', encoding = NULL, col_select = NULL, skip = 0,
                 n_max = Inf, .name_repair = "unique")
+d <- ene$cae_especifico>=8 & ene$cae_especifico<=9
+
+fdt <- ene$cae_especifico>=1 & ene$cae_especifico<=9
 
 # seleccionar variables de interés para generar stock, en base a INE
-    # variables de empleo (activ, r_p_rev4cl_caenes, ocup_form)
-    # variables sociodemográficas (sexo, tramo_edad, region, cine)
+    ## filtrar edad (15 a 29 años)
+    ## variables de empleo (cae_especifico, activ, r_p_rev4cl_caenes, ocup_form)
+    ## variables sociodemográficas (sexo, tramo_edad, region, cine)
 
